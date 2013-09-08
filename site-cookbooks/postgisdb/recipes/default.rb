90834c1054449439d9aa3633aefeeb9b9a1ea9ec
Grant Heffernan <heffergm@gmail.com>
2013-09-08 14:18:06 -0400

    1

--- site-cookbooks/postgisdb/recipes/default.rb
+++ site-cookbooks/postgisdb/recipes/default.rb
@@ -8,7 +8,6 @@
 #
 
 include_recipe 'apt'
-include_recipe 'database'
 
 %w(postgresql postgresql-contrib postgis).each do |p|
   package p do
@@ -16,44 +15,26 @@ include_recipe 'database'
   end
 end
 
-postgresql_database_user 'gisuser' do
-  connection ({:host => "127.0.0.1", :port => 5432, :username => 'postgres'})
-  action :create
-end
+bash 'config-gis-database' do
+  user 'postgres'
+  cwd '/tmp'
+  code <<-EOH
+    sleep 10
+    echo 'Beginning run...'
+    createuser --superuser gisuser
+    createdb -E UTF8 -O gisuser gis
+    createlang plpgsql gis
+    echo "ALTER USER gisuser WITH PASSWORD 'blah';" | psql -d gis
 
-postgresql_database 'gis' do
-  connection ({:host => "127.0.0.1", :port => 5432, :username => 'postgres'})
-  encoding 'UTF8'
-  owner 'gisuser'
-  action :create
-end
+    psql -d gis -f /usr/share/postgresql/9.1/contrib/postgis-1.5/postgis.sql
+    psql -d gis -f /usr/share/postgresql/9.1/contrib/postgis-1.5/spatial_ref_sys.sql
+    psql -d gis -f /usr/share/postgresql/9.1/contrib/postgis_comments.sql
+    psql -d gis -c "GRANT SELECT ON spatial_ref_sys TO PUBLIC;"
+    psql -d gis -c "GRANT ALL ON geometry_columns TO gisuser;"
 
-postgresql_database_user 'gisuser' do
-  connection ({:host => "127.0.0.1", :port => 5432, :username => 'postgres'})
-  database_name 'gis'
-  privileges [:all]
-  action :grant
+    echo 'Touching complete file'
+    touch #{setup_complete}
+  EOH
+  not_if { ::File.exists?(setup_complete) }
 end
-
-#bash 'config-gis-database' do
-#  user 'postgres'
-#  cwd '/tmp'
-#  code <<-EOH
-#    echo 'Beginning run...'
-#    createuser --superuser gisuser
-#    createdb -E UTF8 -O gisuser gis
-#    createlang plpgsql gis
-#    echo "ALTER USER gisuser WITH PASSWORD 'blah';" | psql -d gis
-#
-#    psql -d gis -f /usr/share/postgresql/9.1/contrib/postgis-1.5/postgis.sql
-#    psql -d gis -f /usr/share/postgresql/9.1/contrib/postgis-1.5/spatial_ref_sys.sql
-#    psql -d gis -f /usr/share/postgresql/9.1/contrib/postgis_comments.sql
-#    psql -d gis -c "GRANT SELECT ON spatial_ref_sys TO PUBLIC;"
-#    psql -d gis -c "GRANT ALL ON geometry_columns TO gisuser;"
-#
-#    echo 'Touching complete file'
-#    touch #{setup_complete}
-#  EOH
-#  #not_if { ::File.exists?(setup_complete) }
-#end
 
