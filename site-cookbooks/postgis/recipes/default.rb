d2103b95553eccaae6da0ad81c0bfe3f1f449d24
Grant Heffernan <heffergm@gmail.com>
2013-09-08 09:58:57 -0400

    1

---
+++ site-cookbooks/postgis/recipes/default.rb
@@ -0,0 +1,32 @@
+#
+# Cookbook Name:: postgis
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+%w(postgresql postgresql-contrib postgis).each do |p|
+  package p do
+    action :install
+  end
+end
+
+bash 'config-gis-database' do
+  user 'postgres'
+  cwd '/tmp'
+  code <<-EOH
+    createuser --superuser gisuser
+    createdb -E UTF8 -O gisuser gis
+    createlang plpgsql gis
+    echo "ALTER USER gisuser WITH PASSWORD 'blah';" | psql -d gis
+
+    psql -d gis -f /usr/share/postgresql/9.1/contrib/postgis-1.5/postgis.sql
+    psql -d gis -f /usr/share/postgresql/9.1/contrib/postgis-1.5/spatial_ref_sys.sql
+    psql -d gis -f /usr/share/postgresql/9.1/contrib/postgis_comments.sql
+    psql -d gis -c "GRANT SELECT ON spatial_ref_sys TO PUBLIC;"
+    psql -d gis -c "GRANT ALL ON geometry_columns TO gisuser;"
+  EOH
+end
+
