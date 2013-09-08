3c75c0ec5be15a52acb500e6d47157be9ee2dcd3
Grant Heffernan <heffergm@gmail.com>
2013-09-08 11:45:31 -0400

    1

--- site-cookbooks/postgisdb/recipes/default.rb
+++ site-cookbooks/postgisdb/recipes/default.rb
@@ -15,26 +15,42 @@ include_recipe 'apt'
   end
 end
 
-setup_complete = '/tmp/.PG_SETUP_COMPLETE'
-bash 'config-gis-database' do
-  user 'postgres'
-  cwd '/tmp'
-  code <<-EOH
-    echo 'Beginning run...'
-    createuser --superuser gisuser
-    createdb -E UTF8 -O gisuser gis
-    createlang plpgsql gis
-    echo "ALTER USER gisuser WITH PASSWORD 'blah';" | psql -d gis
-
-    psql -d gis -f /usr/share/postgresql/9.1/contrib/postgis-1.5/postgis.sql
-    psql -d gis -f /usr/share/postgresql/9.1/contrib/postgis-1.5/spatial_ref_sys.sql
-    psql -d gis -f /usr/share/postgresql/9.1/contrib/postgis_comments.sql
-    psql -d gis -c "GRANT SELECT ON spatial_ref_sys TO PUBLIC;"
-    psql -d gis -c "GRANT ALL ON geometry_columns TO gisuser;"
-
-    echo 'Touching complete file'
-    touch #{setup_complete}
+execute 'create-gis-user' do
+  exists = <<-EOH
+  psql -c "select * from pg_user where usename='gisuser'" | grep -c gisuser 
   EOH
-  #not_if { ::File.exists?(setup_complete) }
+  command "createuser --superuser gisuser"
+  not_if exists 
 end
+ 
+execute 'create-database' do
+  user 'postgres'
+  exists = <<-EOH
+  psql -c "select * from pg_database WHERE datname='gis'" | grep -c gis
+  EOH
+  command "createdb -E UTF8 -O gisuser gis && createlang plpgsql gis"
+  not_if exists
+end
+
+#bash 'config-gis-database' do
+#  user 'postgres'
+#  cwd '/tmp'
+#  code <<-EOH
+#    echo 'Beginning run...'
+#    createuser --superuser gisuser
+#    createdb -E UTF8 -O gisuser gis
+#    createlang plpgsql gis
+#    echo "ALTER USER gisuser WITH PASSWORD 'blah';" | psql -d gis
+#
+#    psql -d gis -f /usr/share/postgresql/9.1/contrib/postgis-1.5/postgis.sql
+#    psql -d gis -f /usr/share/postgresql/9.1/contrib/postgis-1.5/spatial_ref_sys.sql
+#    psql -d gis -f /usr/share/postgresql/9.1/contrib/postgis_comments.sql
+#    psql -d gis -c "GRANT SELECT ON spatial_ref_sys TO PUBLIC;"
+#    psql -d gis -c "GRANT ALL ON geometry_columns TO gisuser;"
+#
+#    echo 'Touching complete file'
+#    touch #{setup_complete}
+#  EOH
+#  #not_if { ::File.exists?(setup_complete) }
+#end
 
