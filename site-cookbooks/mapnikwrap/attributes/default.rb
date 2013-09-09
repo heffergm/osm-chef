65a00866d0f18be796ab983c8a2c3264a67db87f
Grant Heffernan <heffergm@gmail.com>
2013-09-09 12:12:09 -0400

    1

--- site-cookbooks/mapnikwrap/attributes/default.rb
+++ site-cookbooks/mapnikwrap/attributes/default.rb
@@ -1,3 +1,3 @@
 override[:mapnik][:ubuntu][:ppa]      = 'v2.2.0'
-override[:mapnik][:ubuntu][:packages] = [ 'libmapnik2-dev', 'python-mapnik2' ]
+override[:mapnik][:ubuntu][:packages] = [ 'libmapnik2-2.0', 'python-mapnik2', 'mapnik-utils' ]
 
